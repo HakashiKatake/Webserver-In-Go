b59386200aefaf7b95e34cd28aaa4a876be77ab2
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-04-22 22:06:13 +0530

    web server with api

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+		// Handler code for listing books
+		json.NewEncoder(w).Encode("Hello world")
+	})
+
+	log.Println("Server started at :8080")
+	http.ListenAndServe(":8080", router)
+}
